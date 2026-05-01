de38d3679bc12e31766c9d12175285a26ab6ea42
Mat Edra <reymart.d.edra@isu.edu.ph>
2026-05-01 10:34:14 +0800

    Use env vars for DB connection (Railway support)

--- include/conn.php
+++ include/conn.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "smart_farming_ph";
+$servername = getenv('DB_HOST') ?: 'localhost';
+$port       = (int)(getenv('DB_PORT') ?: 3306);
+$username   = getenv('DB_USER') ?: 'root';
+$password   = getenv('DB_PASS') ?: '';
+$dbname     = getenv('DB_NAME') ?: 'smart_farming_ph';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
